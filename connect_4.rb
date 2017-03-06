bdb64f17a8577beba29bafd334dbe0b059aa13f4
Auston Bunsen <auston.bunsen@gmail.com>
2017-03-06 16:10:43 -0500

    dealing with moves

--- connect_4.rb
+++ connect_4.rb
@@ -16,6 +16,34 @@ class Connect4
     @board[x][y] = color
   end
 
+  def generate_move
+    # check to see if your next move is a winner
+    return @next_move if next_move_wins
+    # now find move with max prob of winning
+
+  end
+
+  def next_move_wins
+    win = false
+    possible_moves(@board).each do |x, y|
+      sim_board = @board.dup
+
+    end
+  end
+
+  def board_output
+    print_board(@board)
+  end
+
+  def print_board(board)
+    board.each do |row|
+      row.each do |space|
+        print "| #{space||'e'} |"
+      end
+      print "\n"
+    end
+  end
+
   def possible_moves(board)
     row = board.length-1
     moves = []
@@ -36,42 +64,20 @@ class Connect4
     end
     moves
   end
-
-
-
-  def generate_move
-    # check to see if your next move is a winner
-    return @next_move if next_move_wins
-    # now find move with max prob of winning
-
-  end
-
-  def next_move_wins
-    win = false
-    open_slots(@board).each do |x, y|
-      sim_board = @board.dup
-
-    end
-  end
-
-  def winning_board
-  end
-
-  def board_output
-    print_board(@board)
-  end
-
-  def print_board(board)
-    board.each do |row|
-      row.each do |space|
-        print "| #{space||'e'} |"
-      end
-      print "\n"
-    end
-  end
 end
 
 
 b = Connect4.new("red")
 p b.possible_moves(b.board)
 b.board_output
+
+input = gets.chomp
+until input == "q"
+  if input.start_with?("y:") || input.start_with?("r:")
+    color = input.split(":").first
+    x,y = *input.split(":")[1].split(",").map(&:to_i)
+    b.add_move(x,y,color)
+  end
+  b.board_output
+  input = gets.chomp
+end
